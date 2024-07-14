bf2251f210df66291ae1daacb5151ceb55cf60ca
yanjunq <mandyqian58@gmail.com>
2024-07-13 19:40:09 -0700

    fetch data and clean the data, update the analysis_data

--- fetchData.py
+++ fetchData.py
@@ -1,65 +1,63 @@
-import praw
-import pandas as pd
-from datetime import datetime, timezone
+import sys
+from pyspark.sql import SparkSession, functions, types, Row
 
-reddit = praw.Reddit(
-    client_id='zv48-ZAaTgWdcRCv1KMiDw',
-    client_secret='Cbln7P78Q7N65cjv4rcfqdeueDmtcA',
-    user_agent='CMPT353_project'
-)
+spark = SparkSession.builder.appName('reddit extracter').getOrCreate()
 
-# Define functions to grab posts data
-def fetch_posts(subreddit, category, limit=1000):
-    posts_data = []
-    if category == 'top':
-        posts = subreddit.top(time_filter='year', limit=limit)
-    elif category == 'controversial':
-        posts = subreddit.controversial(time_filter='year', limit=limit)
+reddit_submissions_path = '/courses/datasets/reddit_submissions_repartitioned/'
+# reddit_comments_path = '/courses/datasets/reddit_comments_repartitioned/'
+output = 'reddit-subset'
 
-    for post in posts:
-        post_time = datetime.fromtimestamp(post.created_utc, tz=timezone.utc)
-        posts_data.append([
-            post.title,
-            post.selftext,
-            post.score,
-            post.id,
-            post.subreddit.display_name,
-            post.url,
-            post.num_comments,
-            post_time.strftime('%Y-%m-%d %H:%M:%S'),  # UTC time
-            post.author.name if post.author else 'N/A',
-            post.is_self,
-            post.over_18,
-            post.spoiler,
-            post.upvote_ratio,
-            post.ups,
-            post.downs,
-            post_time.strftime('%A'),  # Day of the week
-            post_time.strftime('%H'),  # Hour of the day
-        ])
-    return posts_data
-
-# Grab the first 1000 top posts
-subreddit = reddit.subreddit('all')
-top_posts_data = fetch_posts(subreddit, 'top', limit=1000)
-
-# Convert to a DataFrame and save to CSV
-top_df = pd.DataFrame(top_posts_data, columns=[
-    'title', 'selftext', 'score', 'id', 'subreddit', 'url', 'num_comments',
-    'created_local', 'author', 'is_self', 'over_18', 'spoiler', 'upvote_ratio', 'upvotes', 'downvotes', 'day_of_week', 'hour_of_day'
+submissions_schema = types.StructType([
+    types.StructField('archived', types.BooleanType()),
+    types.StructField('author', types.StringType()),
+    types.StructField('author_flair_css_class', types.StringType()),
+    types.StructField('author_flair_text', types.StringType()),
+    types.StructField('created', types.LongType()),
+    types.StructField('created_utc', types.StringType()),
+    types.StructField('distinguished', types.StringType()),
+    types.StructField('domain', types.StringType()),
+    types.StructField('downs', types.LongType()),
+    types.StructField('edited', types.BooleanType()),
+    types.StructField('from', types.StringType()),
+    types.StructField('from_id', types.StringType()),
+    types.StructField('from_kind', types.StringType()),
+    types.StructField('gilded', types.LongType()),
+    types.StructField('hide_score', types.BooleanType()),
+    types.StructField('id', types.StringType()),
+    types.StructField('is_self', types.BooleanType()),
+    types.StructField('link_flair_css_class', types.StringType()),
+    types.StructField('link_flair_text', types.StringType()),
+    types.StructField('media', types.StringType()),
+    types.StructField('name', types.StringType()),
+    types.StructField('num_comments', types.LongType()),
+    types.StructField('over_18', types.BooleanType()),
+    types.StructField('permalink', types.StringType()),
+    types.StructField('quarantine', types.BooleanType()),
+    types.StructField('retrieved_on', types.LongType()),
+    types.StructField('saved', types.BooleanType()),
+    types.StructField('score', types.LongType()),
+    types.StructField('secure_media', types.StringType()),
+    types.StructField('selftext', types.StringType()),
+    types.StructField('stickied', types.BooleanType()),
+    types.StructField('subreddit', types.StringType()),
+    types.StructField('subreddit_id', types.StringType()),
+    types.StructField('thumbnail', types.StringType()),
+    types.StructField('title', types.StringType()),
+    types.StructField('ups', types.LongType()),
+    types.StructField('url', types.StringType()),
+    types.StructField('year', types.IntegerType()),
+    types.StructField('month', types.IntegerType()),
 ])
-top_df.to_csv('reddit_top_posts.csv', index=False)
-print("saved reddit_top_posts.csv")
 
-# Grab the first 1000 controversial posts
-controversial_posts_data = fetch_posts(subreddit, 'controversial', limit=1000)
+def main():
+    reddit_submissions = spark.read.json(reddit_submissions_path, schema=submissions_schema)
+    # subs = [2024]
+    # subs = list(map(functions.lit, subs))
+    
+    reddit_submissions.where(reddit_submissions['year'] == 2022) \
+        .where(reddit_submissions['score'] >= 50000) \
+        .write.json(output + '/submissions', mode='overwrite', compression='gzip')
 
-# Convert to a DataFrame and save to CSV
-controversial_df = pd.DataFrame(controversial_posts_data, columns=[
-    'title', 'selftext', 'score', 'id', 'subreddit', 'url', 'num_comments',
-    'created_local', 'author', 'is_self', 'over_18', 'spoiler', 'upvote_ratio', 'upvotes', 'downvotes', 'day_of_week', 'hour_of_day'
-])
-controversial_df.to_csv('reddit_controversial_posts.csv', index=False)
-print("saved reddit_controversial_posts.csv")
+if __name__ =='__main__':
 
-
+    main()
